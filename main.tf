b66031385887299d893e091da95a57820be15a91
meghasyam1997 <meghasyam01@gmail.com>
2023-09-20 09:10:38 +0530

    peering connection

--- main.tf
+++ main.tf
@@ -56,3 +56,16 @@ resource "aws_vpc_peering_connection" "peer" {
   vpc_id      = aws_vpc.vpc.id
   auto_accept = true
 }
+
+resource "aws_route" "peering_connection_route" {
+  count                  = length(module.subnets["public"].route_ids)
+  route_table_id         = module.subnets["public"].route_ids[count.index]
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block = var.default_vpc_cidr
+}
+
+resource "aws_route" "peering_connection_route_in_default_vpc" {
+  route_table_id         = var.default_vpc_rtid
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  destination_cidr_block = var.cidr_block
+}
