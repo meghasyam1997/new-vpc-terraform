b0e799bc06340dd899a188fb0af7e3ceefd58fec
meghasyam1997 <meghasyam01@gmail.com>
2024-01-26 13:35:22 +0530

    vpc

--- locals.tf
+++
@@ -1,3 +0,0 @@
-locals {
-  all_private_route_ids = concat(module.subnets["app"].route_ids,module.subnets["db"].route_ids,module.subnets["web"].route_ids)
-}
--- outputs.tf
+++
@@ -1,7 +0,0 @@
-output "subnets" {
-  value = module.subnets
-}
-
-output "vpc_id" {
-  value = aws_vpc.vpc.id
-}
