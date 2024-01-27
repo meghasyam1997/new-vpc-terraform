6dfed1e7a770575881a09cf62224c434ff2f5864
meghasyam1997 <meghasyam01@gmail.com>
2024-01-27 10:55:30 +0530

    adding subnets to vpc

--- main.tf
+++ main.tf
@@ -1,4 +1,16 @@
 resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
-  tags = merge(var.tags , { Name = "${var.env}-vpc" })
+  cidr_block = var.cidr_block
+
+  tags = merge(var.tags, { Name = "${var.env}-vpc" })
+}
+
+module "subnets" {
+  source = "./subnets"
+
+  for_each   = var.subnets
+  vpc_id     = aws_vpc.main.id
+  cidr_block = each.value["cidr_block"]
+  name       = each.value["name"]
+  env        = var.env
+
 }
---
+++ subnets/vars.tf
@@ -0,0 +1,4 @@
+variable "vpc_id" {}
+variable "cidr_block" {}
+variable "env" {}
+variable "name" {}
